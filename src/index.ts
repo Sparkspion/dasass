9777e32b70af6bca1c100dfdb92080abce757530
Sparkspion <008.mnj@gmail.com>
2025-02-14 11:43:26 +0530

    updated configs for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import express from "express";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import cors from "cors";
-import morgan from "morgan";
-import logger from "./utils/logger";
-import mongoose from "mongoose";
-import { errorHandler } from "./middlewares/errorHandler";
-import authRoutes from "./routes/authRoutes";
-import userRoutes from "./routes/userRoutes";
-import healthRoute from "./routes/healthRoute";
-import mongoSanitize from "express-mongo-sanitize";
-import hpp from "hpp";
+import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import logger from './utils/logger';
+import mongoose from 'mongoose';
+import { errorHandler } from './middlewares/errorHandler';
+import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
+import healthRoute from './routes/healthRoute';
+import mongoSanitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
+import MESSAGE from './base/messages';
 
 dotenv.config();
 
@@ -22,27 +23,27 @@ app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
 app.use(
-  morgan("combined", {
+  morgan('combined', {
     stream: {
       write: (msg) => logger.info(msg.trim()),
     },
   })
 );
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/health", healthRoute);
-app.use("/uploads", express.static("uploads"));
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/health', healthRoute);
+app.use('/uploads', express.static('uploads'));
 app.use(mongoSanitize()); // Prevents NoSQL injection
 app.use(hpp()); // Prevents HTTP Parameter Pollution
 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI as string)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log(MESSAGE.MONGO_CONNECTED))
+  .catch((err) => console.error(MESSAGE.MONGO_ERROR, err));
 
-app.get("/", (req, res) => {
-  res.send("Server is running!");
+app.get('*', (_, res) => {
+  res.status(200).send('Hello App');
 });
 
 const PORT = process.env.PORT || 5000;
